591527e2607d5d00bc98655cea51ab6a29c86307
Joshua M Hertlein <jmhertlein@gmail.com>
2014-03-10 19:02:16 -0500

    Fixed the meta energy function to use the template's point format

--- project1/energy.m
+++ project1/energy.m
@@ -1,2 +1,2 @@
-function e = energy(a, b, c, point, points, d, imGrad)
-e = a*gradientEnergy(imGrad, point(1), point(2), y) + b*continuityEnergy(d, points, point) + c * smoothnessEnergy(points, point);
+function e = energy(a, b, c, point, X, Y, d, imGrad)
+e = a*gradientEnergy(imGrad, point(1), point(2), y) + b*continuityEnergy(d, X, Y, point) + c * smoothnessEnergy(X, Y, point);
