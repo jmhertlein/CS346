6099e37c65d901b874809e3076bc5f2a1ea565d9
Joshua M Hertlein <jmhertlein@gmail.com>
2014-03-12 10:16:47 -0500

    U will no longer return points outside of the image

--- project1/U.m
+++ project1/U.m
@@ -1,6 +1,15 @@
 function [X, Y] = U(im, x, y)
-
+[maxY,maxX] = size(im);
 X = [x-2 x-1 x x+1 x+2 x-2 x-1 x x+1 x+2 x-2 x-1 x x+1 x+2 x-2 x-1 x x+1 x+2 x-2 x-1 x x+1 x+2 ];
 Y = [y+2 y+2 y+2 y+2 y+2 y+1 y+1 y+1 y+1 y+1 y y y y y y-1 y-1 y-1 y-1 y-1 y-2 y-2 y-2 y-2 y-2 ];
+
+for i= 1:numel(X)
+    if(X(i) > maxX)
+        X(i) = maxX;
+    end
+    if(Y(i) > maxY)
+        Y(i) = maxY;
+    end
+end
 
 
