8c8999f9541130ef873f4e442f56a8842e95dc84
Joshua M Hertlein <jmhertlein@gmail.com>
2014-03-11 20:42:35 -0500

    Increased the radius of the neighborhood by 1

--- project1/U.m
+++ project1/U.m
@@ -1,4 +1,6 @@
 function [X, Y] = U(im, x, y)
 
-X = [x-1 x x+1 x-1 x x+1 x-1 x x+1];
-Y = [y+1 y+1 y+1 y y y y-1 y-1 y-1];
+X = [x-2 x-1 x x+1 x+2 x-2 x-1 x x+1 x+2 x-2 x-1 x x+1 x+2 x-2 x-1 x x+1 x+2 x-2 x-1 x x+1 x+2 ];
+Y = [y+2 y+2 y+2 y+2 y+2 y+1 y+1 y+1 y+1 y+1 y y y y y y-1 y-1 y-1 y-1 y-1 y-2 y-2 y-2 y-2 y-2 ];
+
+
