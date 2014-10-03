c8d38f7120a7c5c7760be4c5f8dd09c7cc68ac07
Joshua Michael Hertlein <jmhertlein@gmail.com>
2014-10-03 11:42:59 -0500

    Added some explanatory comments for the X,Y arrays

--- project1/U.m
+++ project1/U.m
@@ -1,5 +1,15 @@
 function [X, Y] = U(im, x, y)
 [maxY,maxX] = size(im);
+
+% The coords for a 5x5 patch around a given point
+% X and Y together are the x's and y coords of ordered pairs
+% e.g.
+%
+% XXXXX <- (X(5), Y(5))
+% XXXXX
+% XXOXX
+% XXXXX
+% XXXXX
 X = [x-2 x-1 x x+1 x+2 x-2 x-1 x x+1 x+2 x-2 x-1 x x+1 x+2 x-2 x-1 x x+1 x+2 x-2 x-1 x x+1 x+2 ];
 Y = [y+2 y+2 y+2 y+2 y+2 y+1 y+1 y+1 y+1 y+1 y y y y y y-1 y-1 y-1 y-1 y-1 y-2 y-2 y-2 y-2 y-2 ];
 
